e67c0ff0a70bb2ba01c7c7cb35da104434896322
COMPIT\yavgeniy.lang <yavgeniy.lang@compit.by>
2022-08-16 11:48:07 +0300

    1

---
+++ src/main/java/by/example/swimming_pool/SwimmingPoolApplication.java
@@ -0,0 +1,13 @@
+package by.example.swimming_pool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwimmingPoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwimmingPoolApplication.class, args);
+	}
+
+}
